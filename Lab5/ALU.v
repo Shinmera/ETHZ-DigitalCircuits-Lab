4d8a601b6b8df907801455e6a5735082236e7c8f
Shinmera <shinmera@tymoon.eu>
2015-04-24 15:32:47 +0200

    Fix ALU

--- Lab5/ALU.v
+++ Lab5/ALU.v
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
 
 module ALU(
-           input [31:0]  A,
-           input [31:0]  B,
-           input [3:0]   AluOP,
+           input [31:0]      A,
+           input [31:0]      B,
+           input [3:0]       AluOP,
            output reg [31:0] out,
-           output        zero
+           output            zero
            );
    
    assign zero = (out == 32'b0);
@@ -18,7 +18,7 @@ module ALU(
         4'b0101: out <= A | B;
         4'b0110: out <= A ^ B;
         4'b0111: out <= ~(A | B);
-        4'b1010: out <= {{31{1'b0}}, (A-B)}; // (A-B) >> 31
+        4'b1010: out <= (( (~B) + 32'b1) + A) >> 31; // (A-B) >> 31
         default: out <= 32'b0;
       endcase // case (AluOp)
    end // always @ (*)
