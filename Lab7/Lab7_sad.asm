f772dfd7c53f9fe397c0934d63f4b9fc79c1348d
Shinmera <shinmera@tymoon.eu>
2015-05-08 23:44:45 +0200

    Cleaned up and working version

--- Lab7/Lab7_sad.asm
+++ Lab7/Lab7_sad.asm
@@ -1,17 +1,34 @@
-#
-# Sum of Absolute Differences Algorithm
-#
-# Authors: 
-#	X Y, Z Q 
-#
-# Group: ...
-#
+### Sum of Absolute Differences Algorithm
+# Authors: Nicolas Hafner
+# Group: F32
 
 .text
+
+## Implicitly fixed registers:
+# $s0 left_image
+# $s1 right_image
+# $s2 sad_image
+# We do this since we don't have enough return and argument
+# registers, and keeping them all intact over jumps would be
+# a pain.
+
 main:
-	lui     $t1, 0x0000 # Address of first element in the vector
+        jal     memory_init
+
+        li      $a0, 9
+        jal     calculate_sad
+
+        move    $a0, $s2
+        li      $a1, 9
+        jal     recursive_sum
+
+        move    $t2, $v0
+        j       end
+
+memory_init:
+	lui     $t1, 0x0000
 	ori     $t1, 0x0000
-        
+
         #left starting at $s0
         move    $s0, $t1
         # left_image[0]
@@ -92,69 +109,84 @@ main:
 
         # target starting at $s2
         move    $s2, $t1
+        # implicit zero allocation
 
-loop_init:
+        jr      $ra
+
+calculate_sad:
 	li      $s3, 0
-	li      $s4, 9
+	move    $s4, $a0
+        li      $t0, 4
+        mul     $s4, $s4, $t0
+        j       sad_body
 
-loop:
-	beq     $s3, $s4, loop_end
-	
-	move    $t1, $s0
-        add     $t1, $t1, $s3
-        lw      $a0, ($t1)
-	
-        move    $t1, $s1
-        addi    $t1, $t1, $s3
-        lw      $a1, ($t1)
-	
-	jal     abs_diff
+sad_body:
+	beq     $s3, $s4, branch_ra
 
-        move    $t1, $s2
-        addi    $t1, $t1, $s3
-        sw      $v0, ($t1)
-        
-        addi    $s1, $s1, 1
-	j       loop
-	
-loop_end:
-        move    $a0, $s2
-        move    $a1, $s4
-        jal     recursive_sum
+        add     $t0, $s0, $s3
+        lw      $a0, ($t0)
 
-        move    $t2, $v0
+        add     $t0, $s1, $s3
+        lw      $a1, ($t0)
 
-end:	
-	j	end
+        move    $t0, $ra
+        la      $t1, abs_diff
+	jal     jump_preserve
+
+        add     $t0, $s2, $s3
+        sw      $v0, ($t0)
+
+        addi    $s3, $s3, 4
+	j       sad_body
 
 abs_diff:
         sub     $v0, $a0, $a1
-        bgtz    $v0, abs_diff_return
-        li      $t1, -1
-        mul     $v0, $v0, $t1
-
-abs_diff_return:
+        bgtz    $v0, branch_ra
+        li      $t0, -1
+        mul     $v0, $v0, $t0
         jr      $ra
 
 recursive_sum:
-        addi    $sp, $sp, -8
         addi    $a1, $a1, -1
-        sw      $a1, ($sp)
-        sw      $ra, 4($sp)
         bgtz    $a1, recursive_sum_calc
         li      $v0, 0
-        addi    $sp, $sp, 8
         jr      $ra
 
 recursive_sum_calc:
-        jal     recursive_sum
-        lw      $a1, ($sp)
-        lw      $ra, 4($sp)
-        li      $t7, 4
-        mul     $t1, $a1, $t7
-        add     $t1, $t1, $a0
-        lw      $t2, ($t1)
-        add     $v0, $v0, $t2
-        addi    $sp, $sp, 8
+        move    $t0, $ra
+        la      $t1, recursive_sum
+        jal     jump_preserve
+
+        li      $t0, 4
+        mul     $t0, $t0, $a1
+        add     $t0, $t0, $a0
+        lw      $t0, ($t0)
+        add     $v0, $v0, $t0
         jr      $ra
-        
+
+## Explanation of jump_preserve:
+# Jumps while preserving $ra, $a0, $a1 and the callee's $ra. We do
+# this by conventionally requiring the original $ra be stored in $t0.
+# The value in $t1 is used as the label to jump to. Thus a call to
+# jump_preserve will always first move the $ra to $t0 and set $t1 to
+# the desired label.
+jump_preserve:
+        addi    $sp, $sp, -16
+        sw      $ra, ($sp)
+        sw      $t0, 4($sp)
+        sw      $a0, 8($sp)
+        sw      $a1, 12($sp)
+        jalr    $t1
+        lw      $t0, ($sp)
+        lw      $ra, 4($sp)
+        lw      $a0, 8($sp)
+        lw      $a1, 12($sp)
+        addi    $sp, $sp, 16
+        jr      $t0
+
+# Convenience to jump to the return address on branching.
+branch_ra:
+        jr      $ra
+
+end:
+	j	end
